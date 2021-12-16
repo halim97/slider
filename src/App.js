6d68240a720c96c038808bfac340e4214fbe2fb0
halim97 <halim.boulberdaa@univ-constantine2.dz>
2021-12-17 00:52:40 +0100

    slider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import people from './data'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+
 
 function App() {
+
+  const[index,setIndex]=useState(0);
+  const left=()=>{
+    
+    if(index===0){
+      setIndex(people.length-1)
+    }else{
+      setIndex(index-1);
+    }
+  }
+  const right=()=>{
+    if(index===people.length-1){
+      setIndex(0)
+    }else{
+      setIndex(index+1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className='App'>
+       { people.map(
+          (person,indexMap)=>{
+              const {id,image,name,title,quote}=person;
+              let position='next';
+              if (indexMap===index){
+                position='actuel';
+              }
+              if(indexMap===(index-1)||index===0&&indexMap===people.length-1){
+                  position='prev';
+              }
+
+            return(
+              
+              <article className={position} key={indexMap}>
+              
+              <img className='image' src={image} alt={name}/>
+              <h3 className='name a'>{name}</h3>
+              <h4 className='title a'>{title}</h4>
+              <p className='quote a'>{quote}</p>
+              <button className='button left' onClick={left}>{<FiChevronLeft/>}</button>
+              <button className='button right' onClick={right}>{<FiChevronRight/>}</button>
+            
+            </article>
+            
+            );
+          }
+
+            )}</div>
+      
+    
+    
   );
 }
 
